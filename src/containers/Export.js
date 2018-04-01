3aa9909620a8285f9b76a0111b600e59563f97ed
Ian Mayo <ian@planetmayo.com>
2018-04-01 15:01:57 +0100

    fix Linter errors

--- src/containers/Export.js
+++ src/containers/Export.js
@@ -1,12 +1,12 @@
-import { connect } from 'react-redux';
-import ExportPdf from '../components/ExportPdf';
-import { getVisibleEntries } from './VisibleEntryList';
-import jsPDF from 'jspdf';
+import { connect } from "react-redux";
+import { getVisibleEntries } from "./VisibleEntryList";
+import ExportPdf from "../components/ExportPdf";
+import jsPDF from "jspdf";
 
 const lineWidth = 195;
 const maxLines = 36;
 
-const groupEntries = (state) => {
+const groupEntries = state => {
   let entries = getVisibleEntries(
     state.entries,
     state.visibilityFilter,
@@ -16,56 +16,56 @@ const groupEntries = (state) => {
     state.searchKeyword
   );
 
-  const pdf = new jsPDF("p", "mm", "a4");
+  const pdf = new jsPDF("p", "mm", "a4"); // eslint-disable-line new-cap
 
   entries = entries.map(entry => {
-    entry.linePoint = 1;
+    const theE = entry;
 
-    let textArray = [];
+    theE.linePoint = 1;
 
-    if(entry.fields)
-      Object.keys(entry.fields).forEach((field, key) => {
-        textArray.push(`${field}: ${entry.fields[field]}`)
-      })
-    ;
+    const textArray = [];
 
-    entry.fieldsFormatted = null;
-
-    if(textArray.length) {
-      entry.fieldsFormatted = pdf.splitTextToSize(textArray.join(', '), lineWidth);
-      entry.linePoint += entry.fieldsFormatted.length;
+    if (theE.fields) {
+      /* eslint-disable-next-line */
+      Object.keys(theE.fields).forEach((field, key) => {
+        textArray.push(`${field}: ${theE.fields[field]}`);
+      });
     }
 
-    return entry;
+    theE.fieldsFormatted = null;
+
+    if (textArray.length) {
+      theE.fieldsFormatted = pdf.splitTextToSize(
+        textArray.join(", "),
+        lineWidth
+      );
+      theE.linePoint += theE.fieldsFormatted.length;
+    }
+
+    return theE;
   });
 
-  let pages = [];
+  const pages = [];
 
-  for(let i = 0, line=0, pageInd = 0; i < entries.length; i++) {
-    if(!pages[pageInd])
-      pages[pageInd] = []
-    ;
+  for (let i = 0, line = 0, pageInd = 0; i < entries.length; i++) {
+    if (!pages[pageInd]) pages[pageInd] = [];
 
     pages[pageInd].push(entries[i]);
     line += entries[i].linePoint;
 
-    if(entries[i+1] && entries[i+1].linePoint + line > maxLines) {
+    if (entries[i + 1] && entries[i + 1].linePoint + line > maxLines) {
       pageInd++;
-      line=0;
+      line = 0;
     }
   }
 
   return pages;
-}
-
-const mapStateToProps = state => {
-  return {
-    entries: groupEntries(state)
-  }
 };
 
-const Export = connect(
-    mapStateToProps
-)(ExportPdf);
+const mapStateToProps = state => ({
+  entries: groupEntries(state)
+});
+
+const Export = connect(mapStateToProps)(ExportPdf);
 
 export default Export;
